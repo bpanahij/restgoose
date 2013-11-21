7674d31d23677d88d8c0555b32c84ac894e4564b
Brian P. Johnson <bjohnson@trulia.com>
2013-11-21 12:43:19 -0800

    -Fixing PUT issues, and adding in meta data for paging queries

--- restgoose.js
+++ restgoose.js
@@ -1,21 +1,15 @@
-/**
- * See MIT License *in footer
- *
- */
 var express = require('express')
   , api = express()
   , http = require('http')
   , fs = require('fs')
   , mongoose = require('mongoose')
-  , _ = require('underscore');
-/**
- * Connect with Mongo DB
- */
+  , _ = require('underscore')
+  , querystring = require('querystring');
+
 api.use(express.methodOverride());
 /**
  * Parse the JSON body
  */
-var secret = 'Passport is your ticket to better colleges and students.';
 api.use(express.compress());
 api.use(express.bodyParser());
 /**
@@ -38,23 +32,69 @@ var buildEndpoints = function(modelDir) {
      * @param res
      */
     var getFunc = function(req, res) {
-      model.findById(req.params.id, function(err, gotModel) {
+      var query = {
+        _id: req.params.id
+      };
+      query = _.extend(query, req.restrictQuery);
+      model.find(query, function(err, gotModel) {
         res.json(gotModel);
       });
     };
+    /**
+     * RESTful query
+     */
     var queryFunc = function(req, res) {
-      var query = model.find();
-      advancedQueries(query, req.query);
+      var parentRoute = req.app.route
+        , queryVars = _.extend(req.query, req.restrictQuery)
+        , query = model.find();
+      advancedQueries(query, queryVars);
+      var pagelessQuery = query.toConstructor()
+        , limit = queryVars.limit
+        , offset = queryVars.offset;
+      // Later is now, handle limit and offset
+      if (_.isUndefined(limit)) {
+        queryVars.limit = limit = 10;
+      }
+      query.limit(limit);
+      if (_.isUndefined(offset)) {
+        queryVars.offset = offset = 0;
+      }
+      query.skip(offset);
       query.exec(function(err, result) {
         if (err) {
           res.json({error: err});
           return;
         }
-        res.json(result);
+        // Count total results
+        pagelessQuery().count().exec(function(err, count) {
+          queryVars.limit = Number(limit);
+          var nextPage = _.clone(queryVars)
+            , prevPage = _.clone(queryVars)
+            , nextOffset = Number(offset) + Number(limit)
+            , prevOffset = Number(offset) - Number(limit)
+            , meta = {};
+          if (nextOffset <= (count - 1)) {
+            nextPage.offset = nextOffset;
+            meta.nextPageURL = parentRoute + '/' + model.modelName + '?' + querystring.stringify(nextPage);
+            meta.nextPageQuery = nextPage;
+          }
+          if (prevOffset > 0) {
+            prevPage.offset = prevOffset;
+            meta.prevPageURL = parentRoute + '/' + model.modelName + '?' + querystring.stringify(prevPage);
+            meta.prevPageQuery = prevPage;
+          }
+          meta.count = count;
+          meta.pages = Math.ceil(count / limit);
+          meta.page = Math.ceil(offset / limit) + 1;
+          res.json({
+            meta: meta,
+            result: result
+          });
+        });
       });
     };
-    var advancedQueries = function(query, queries) {
-      _.each(queries, function(value, key) {
+    var advancedQueries = function(query, queryVars) {
+      _.each(queryVars, function(value, key) {
         if (key.match(/>/)) {
           var cleanKey = key.replace(/>/, '');
           query.gte(cleanKey, value);
@@ -69,11 +109,8 @@ var buildEndpoints = function(modelDir) {
           var reg = new RegExp(regStr, 'i');
           query.regex(cleanKey, reg);
         }
-        else if (key.match('limit')) {
-          query.limit(value);
-        }
-        else if (key.match('offset')) {
-          query.skip(value);
+        else if (key.match('limit') || key.match('offset')) {
+          // skip limit and offset for later
         }
         else if (key.match(/\!/)) {
           var cleanKey = key.replace(/\!/, '');
@@ -90,9 +127,10 @@ var buildEndpoints = function(modelDir) {
      * @param res
      */
     var postFunc = function(req, res) {
-      var body = req.body;
-      if (body._id) {
-        res.json({error: 'Object must be a new ' + model.modelName});
+      var body = _.extend(req.body, req.restrictQuery);
+      if (!_.isUndefined(body._id)) {
+        // Escape into put functionality when there exists a resource id
+        putFunc(req, res);
         return;
       }
       var newModel = new model(body);
@@ -110,16 +148,16 @@ var buildEndpoints = function(modelDir) {
      * @param res
      */
     var putFunc = function(req, res) {
-      var body = req.body;
+      var body = _.extend(req.body, req.restrictQuery);
       var savedModel = new model(body);
       var upsertData = savedModel.toObject();
       delete upsertData._id;
-      model.update ({_id: savedModel._id}, upsertData, {upsert: true}, function (err) {
+      model.update({_id: savedModel._id}, upsertData, {}, function(err) {
         if (err) {
           res.json({error: err});
           return;
         }
-        res.json (savedModel);
+        res.json(savedModel);
       });
     };
     var deleteFunc = function(req, res) {
@@ -137,7 +175,7 @@ var buildEndpoints = function(modelDir) {
     var getEnd = '/' + model.modelName + '/:id';
     var queryEnd = '/' + model.modelName;
     var postEnd = '/' + model.modelName;
-    var putEnd = '/' + model.modelName + '/:id';
+    var putEnd = '/' + model.modelName;
     var deleteEnd = '/' + model.modelName + '/:id';
     api.get(getEnd, getFunc);
     api.get(queryEnd, queryFunc);
